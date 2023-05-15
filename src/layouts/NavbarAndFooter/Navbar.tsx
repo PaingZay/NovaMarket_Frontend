6a14f9c6b3e15788c04305f8d76ea7410c0e30ba
PaingZay <pyne469@gmail.com>
2023-05-16 01:03:23 +0800

    Authentication

--- src/layouts/NavbarAndFooter/Navbar.tsx
+++ src/layouts/NavbarAndFooter/Navbar.tsx
@@ -1,7 +1,21 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
+import { useOktaAuth } from "@okta/okta-react";
+import { SpinnerLoading } from "../Utils/SpinnerLoading";
+import OktaAuth from "@okta/okta-auth-js";
 
 function Navbar () {
+
+  const {oktaAuth, authState} = useOktaAuth();
+
+  if(!authState) {
+    return <SpinnerLoading/>
+  }
+
+  const handleLogout = async() => oktaAuth.signOut();
+
+  console.log(authState);
+
     return (
         <nav className='navbar navbar-expand-lg navbar-dark main-color py-3'>
       <div className='container-fluid'>
@@ -35,16 +49,15 @@ function Navbar () {
             {/* } */}
           </ul>
           <ul className='navbar-nav ms-auto'>
-            {/* {!authState.isAuthenticated ? */}
+            {!authState.isAuthenticated ?
               <li className='nav-item m-1'>
-                <a href="#" className='btn btn-outline-light' type='button'>Sign In</a>
-                {/* <Link type='button' className='btn btn-outline-light' to='/login'>Sign in</Link> */}
+                <Link type='button' className='btn btn-outline-light' to='/login'>Sign in</Link>
               </li>
-              {/* :
+              :
               <li>
                 <button className='btn btn-outline-light' onClick={handleLogout}>Logout</button>
-              </li> */}
-            {/* } */}
+              </li>
+            }
           </ul>
         </div>
       </div>
